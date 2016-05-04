9604090ce73d7eb291bb129ff3afb81585054424
Cody Wahl <codywahl@gmail.com>
2016-05-04 02:47:36 -0700

    Prepped collab branch

--- ErichsTutorialConsoleApp/ErichsTutorialConsoleApp/Program.cs
+++ ErichsTutorialConsoleApp/ErichsTutorialConsoleApp/Program.cs
@@ -7,6 +7,18 @@ namespace ErichsTutorialConsoleApp
         private static void Main(string[] args)
         {
             Console.WriteLine("Konnichiwa!");
+
+            // Erich, please uncomment the following lines. 
+            //var i = 0;
+
+            //do
+            //{
+            //    Console.WriteLine(i.ToString());
+            //    i++;
+            //}
+            //while (i < 10);
+            
+
             Console.ReadKey();
         }
     }
