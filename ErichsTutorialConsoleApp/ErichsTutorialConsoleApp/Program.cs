11022d1595ee7feb17e87fc2b40998f597f6a83b
Cody Wahl <codywahl@gmail.com>
2016-05-04 02:30:28 -0700

    Started the application
    It's an application!

---
+++ ErichsTutorialConsoleApp/ErichsTutorialConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ErichsTutorialConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
